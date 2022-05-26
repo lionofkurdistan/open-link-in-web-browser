ac015f097ca10785a970cbd4b8a2e0125d0c65bb
Dev Lion Of Kurdistan <76536335+lionofkurdistan@users.noreply.github.com>
2022-05-26 10:16:29 +0300

    Add files via upload

---
+++ open link in web browser/main.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+webbrowser.open("www.facbook.com")
+webbrowser.open("www.telegram.com")
+webbrowser.open("www.discord.com")
+webbrowser.open("www.instagram.com")
+webbrowser.open("www.tiktok.com")
